be65906af86b9a850391ad6953a85aedc2cd5680
Branko Majic <branko@majic.rs>
2013-12-27 22:50:35 +0100

    DJPYD-1: Configured the sample project to include Django Pydenticon.

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -3,10 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+# Django Pydenticon.
+import django_pydenticon.urls
+
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'testproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # URLs for Django Pydenticon.
+    url(r'^identicon/', include(django_pydenticon.urls)),
 )
