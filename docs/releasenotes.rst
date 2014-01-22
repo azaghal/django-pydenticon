064b10db6598c4776b45329949346d4a6ccad3ea
Branko Majic <branko@majic.rs>
2014-01-22 22:18:22 +0100

    DJPYD-2: Added build manifest and setup script. Added release notes.

---
+++ docs/releasenotes.rst
@@ -0,0 +1,16 @@
+Release notes
+=============
+
+0.1
+---
+
+Initial release of Django Pydenticon. Implemented features:
+
+* Serving of identicons through designated URL.
+* User data for generating identicons passed via URL.
+* Sane configuration defaults for identicon generator for zero-configuration.
+* Ability to set parameters of generated identicons.
+* Ability to override some of the identicon generation attributes per-request
+  via *GET* parameters.
+* Full documentation covering installation, usage, privacy. API reference
+  included as well.
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+INSTALL_REQUIREMENTS = ["pydenticon"]
+TEST_REQUIREMENTS = []
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-pydenticon',
+    version='0.1',
+    packages=find_packages(exclude=["testproject", "testproject.*"]),
+    include_package_data=True,
+    license='BSD',
+    description='Django application providing generated identicons to the web.',
+    long_description=README,
+    url='https://github.com/azaghal/django-pydenticon',
+    author='Branko Majic',
+    author_email='branko@majic.rs',
+    install_requires=INSTALL_REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
